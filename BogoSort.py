509848cf4c805ee27aaf01a27a2f3e0e41c39a1d
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-02-19 00:13:34 +0530

    BogoSort
    BogoSort added

---
+++ BogoSort.py
@@ -0,0 +1,13 @@
+import pickle
+import random
+
+list = pickle.load( open( "save.p", "rb" ) )
+print(f"{len(list)} values")
+
+while True:
+    list1 = random.shuffle(list)
+    if list1 != sorted(list): 
+        print("Not sorted")
+        pass
+    else:
+        print("Sorted")
